0c10dc2199703de96f140f070a13c07c209af2a3
정연화 <laboratory6889@gmail.com>
2023-09-07 16:35:25 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/travote/travote/TravoteApplication.java
@@ -0,0 +1,13 @@
+package com.travote.travote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travote/travote/TravoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travote.travote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
